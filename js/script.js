7eb25efd8f85dc78bff5505617f2a6aa23a82a20
PratikTandel <pratik.tandel@1rivet.com>
2022-12-08 11:56:25 +0530

    feat: validate ip-address

--- js/script.js
+++ js/script.js
@@ -46,5 +46,20 @@ const getSearchData = (searchTerm) => {
 /** get searchTerm on btn click and call api to fetch data */
 const onSearch = () => {
   const searchTerm = document?.querySelector("#search")?.value;
-  getSearchData(searchTerm);
+  if (validateIPaddress(searchTerm)) {
+    getSearchData(searchTerm);
+  }
 };
+
+/** validate the entered ip address */
+function validateIPaddress(ipaddress) {
+  if (
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+      ipaddress
+    )
+  ) {
+    return true;
+  }
+  alert("You have entered an invalid IP address!");
+  return false;
+}
